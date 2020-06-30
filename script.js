4f4aaffa798b6903685b91f7b91fa4df6b90a794
Alisson K. de Carvalho <alisson.kelvindecarvalho@hotmail.com>
2020-06-30 17:04:08 -0300

    botão de maximizar concluido

--- script.js
+++ script.js
@@ -2,12 +2,13 @@
 
 const modalOverlay = document.querySelector('.modal-overlay'); // pegando a seção inteira.
 const cursos = document.querySelectorAll('.curso');// pega todos os cursos
-
+const  modal = document.querySelector('.modal');
 
 cursos.forEach(curso => { //percorrendo o array de cursos
         curso.addEventListener('click', function(){
 
             const idCurso = curso.getAttribute('id');
+ 
 
             modalOverlay.classList.add('active'); //acrescenta a classe active p/ ativar o modal
             modalOverlay.querySelector('iframe').src= `https://rocketseat.com.br/${idCurso}`; //poem o video dinamicamente no modal
@@ -17,6 +18,17 @@ cursos.forEach(curso => { //percorrendo o array de cursos
 modalOverlay.querySelector('.close').addEventListener('click', function(){ //função que fecha o modal
 
     modalOverlay.classList.remove('active') //remove a classe active 
+
+});
+
+modal.querySelector('.modal span').addEventListener('click', function(){
+
+    if(modal.classList.contains('maximize')== true){
+
+        modal.classList.remove('maximize');
+    }else{
+        modal.classList.add('maximize');
+    }
 
 });
 
