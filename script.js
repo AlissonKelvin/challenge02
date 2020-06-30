97a785d1d2d9a5e5e2dc0cd68855d5640fb44a53
Alisson K. de Carvalho <alisson.kelvindecarvalho@hotmail.com>
2020-06-30 12:36:28 -0300

    Modal adicionado

---
+++ script.js
@@ -0,0 +1,22 @@
+/* JAVASCRIPT */
+
+const modalOverlay = document.querySelector('.modal-overlay'); // pegando a seção inteira.
+const cursos = document.querySelectorAll('.curso');// pega todos os cursos
+
+
+cursos.forEach(curso => { //percorrendo o array de cursos
+        curso.addEventListener('click', function(){
+
+            const idCurso = curso.getAttribute('id');
+
+            modalOverlay.classList.add('active'); //acrescenta a classe active p/ ativar o modal
+            modalOverlay.querySelector('iframe').src= `https://rocketseat.com.br/${idCurso}`; //poem o video dinamicamente no modal
+        });
+});
+
+modalOverlay.querySelector('.close').addEventListener('click', function(){ //função que fecha o modal
+
+    modalOverlay.classList.remove('active') //remove a classe active 
+
+});
+
